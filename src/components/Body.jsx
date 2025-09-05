1db69dcbd8b9a3ddc2053c3e36405e1d492d70e5
Puja0208 <pooja2081997@gmail.com>
2025-09-05 15:47:31 +0530

    feat: Redirect to logn igf not logged in, add linkt o navbar profile

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import { useEffect } from "react";
+import axios from "axios";
 import NavBar from "./NavBar";
-import { Outlet } from "react-router";
+import { Outlet, useNavigate } from "react-router";
 import Footer from "./Footer";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((state) => state.user);
+
+  const fetchUser = async () => {
+    if (userData) return;
+    try {
+      const res = await axios.get(`${BASE_URL}/profile/view`, {
+        withCredentials: true,
+      });
+
+      dispatch(addUser(res.data));
+    } catch (error) {
+      if (error.status === 401) {
+        navigate("/login");
+      }
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div>
       <NavBar />
