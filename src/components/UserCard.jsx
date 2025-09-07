e4da43532d9e8f02eaab6cbe7b62e9c4bf20c2e3
Puja0208 <pooja2081997@gmail.com>
2025-09-07 13:00:41 +0530

    feat: Add ignore/interested functionality

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,6 +1,27 @@
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { removeUserFromFeed } from "../utils/feedSlice";
+
 const UserCard = (props) => {
   const { user } = props;
-  const { firstName, lastName, age, gender, about, photoUrl } = user;
+  const dispatch = useDispatch();
+  const { firstName, lastName, age, gender, about, photoUrl, _id } = user;
+
+  const handleSendRequest = async (status, userId) => {
+    try {
+      const res = await axios.post(
+        `${BASE_URL}/request/send/${status}/${userId}`,
+        {},
+        {
+          withCredentials: true,
+        }
+      );
+      dispatch(removeUserFromFeed(userId));
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className="card bg-base-300 w-96 shadow-sm">
@@ -14,8 +35,18 @@ const UserCard = (props) => {
         {age && gender && <p>{age + ", " + gender}</p>}
         <p>{about}</p>
         <div className="card-actions justify-center my-4">
-          <button className="btn btn-primary">Ignore</button>
-          <button className="btn btn-secondary">Interested</button>
+          <button
+            className="btn btn-primary"
+            onClick={() => handleSendRequest("ignored", _id)}
+          >
+            Ignore
+          </button>
+          <button
+            className="btn btn-secondary"
+            onClick={() => handleSendRequest("interested", _id)}
+          >
+            Interested
+          </button>
         </div>
       </div>
     </div>
