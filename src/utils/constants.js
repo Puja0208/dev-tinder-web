0cc4ac0df919c4bd0bf97dd583fe33e561e61c5d
Puja0208 <pooja2081997@gmail.com>
2025-09-23 07:17:11 +0530

    fix:define base url as per hostname

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,4 +2,5 @@
 // export const BASE_URL = "/api";
 
 //in dev
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL =
+  location.hostname === "localhost" ? "http://localhost:3000" : "/api";
