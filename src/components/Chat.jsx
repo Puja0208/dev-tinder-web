a404595a33811691c9bcd91a1eb30156046f17be
Puja0208 <pooja2081997@gmail.com>
2025-09-30 06:23:58 +0530

    feat: Create chat component UI and layout

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -4,7 +4,16 @@ import { useParams } from "react-router";
 const Chat = () => {
   const { targetUserId } = useParams();
 
-  return <div></div>;
+  return (
+    <div className="w-1/2 mx-auto border border-gray-600 m-5 h-[70vh] flex flex-col">
+      <h1 className="p-5 border-b border-gray-600">Chat</h1>
+      <div className="flex-1 overflow-scroll p-5">{/**Display messages */}</div>
+      <div className="p-5 border-t border-grey-600 flex gap-2 items-center">
+        <input className="flex-1 border border-grey-500 text-white p-5rounded"></input>
+        <button className="btn btn-secondary">Send</button>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
