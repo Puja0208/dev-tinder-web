e46404505712507494637c1653310af88e3f36db
Puja0208 <pooja2081997@gmail.com>
2025-09-30 08:06:25 +0530

    feat: Pass jwt token in socket auth for authentication

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,13 @@
 import { io } from "socket.io-client";
 import { BASE_URL } from "./constants";
+import Cookies from "js-cookie";
+
+const token = Cookies.get("token");
 
 export const createSocketConnection = () => {
-  return io(BASE_URL);
+  return io(BASE_URL, {
+    auth: {
+      token: token,
+    },
+  });
 };
